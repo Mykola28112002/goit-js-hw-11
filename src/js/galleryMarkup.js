ee2b8c3c3756c5d1f1e2b338086b90a0db606fe9
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-07-09 22:01:31 +0300

    1

--- src/js/galleryMarkup.js
+++ src/js/galleryMarkup.js
@@ -35,8 +35,12 @@ function makeGallaryMarkup(searchedImages) {
                   <b>${comments}</b>
                 </p>
                 <p class="info-item">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-arrow-down-fill" viewBox="0 0 16 16">
-  <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z"/>
+                <svg aria-hidden="true" style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<defs>
+<symbol id="icon-comment" viewBox="0 0 18 18">
+<path d="M8.982 2.25c-3.092 0-5.607 2.523-5.607 5.625s2.516 5.625 5.591 5.625l0.067-0.004c0.062-0.004 0.124-0.008 0.186-0.011l0.378-0.016 4.123 1.236-0.708-2.793 0.581-0.841c0.652-0.944 0.997-2.050 0.997-3.197 0-3.102-2.515-5.625-5.607-5.625zM8.982 0c4.34 0 7.857 3.526 7.857 7.875 0 1.664-0.517 3.204-1.396 4.476l1.432 5.649-7.562-2.267c-0.11 0.005-0.219 0.017-0.33 0.017-4.34 0-7.857-3.526-7.857-7.875s3.518-7.875 7.857-7.875v0z"></path>
+</symbol>
+</defs>
 </svg>
                   <b>${downloads}</b>
                 </p>
